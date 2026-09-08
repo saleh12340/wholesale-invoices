e2bcf1c2b5593f1366df72a8a3b81b2d2834a9ef
saleh12340 <salthbiany@gmail.com>
2026-09-08 22:50:34 +0300

    fix: activate native export and WhatsApp image handling

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { startCustomerInvoicesEnhancer } from './utils/customerInvoicesEnhancer';
+import { startExportAndShareEnhancer } from './utils/exportAndShareEnhancer';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -11,3 +12,4 @@ createRoot(document.getElementById('root')!).render(
 );
 
 startCustomerInvoicesEnhancer();
+startExportAndShareEnhancer();
